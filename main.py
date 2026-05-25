3c4f4b27a5cf811cec5d59945d81941b17c9a496
guilhermeyuri01 <keviguu@gmail.com>
2026-05-25 00:11:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TOKEN = "8993898662:AAHUS-UCvA8B59twZmS4ozrPFU16UrjH-Ww"
 
 async def receber(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    link = update.message.text
-
-    texto = f"""
+    texto = """
 🔥 OFERTA TECH 🔥
 
-🛒 Produto encontrado:
-{link}
-
-💰 Melhor preço encontrado!
+💰 Bot funcionando!
 """
 
     await update.message.reply_text(texto)
